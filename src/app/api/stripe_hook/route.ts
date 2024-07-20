189445aafef1410f1965622bf93ed191670e6474
serhiihaniuk <serhii.haniuk@gmail.com>
2024-07-20 21:51:44 +0200

    add db await

--- src/app/api/stripe_hook/route.ts
+++ src/app/api/stripe_hook/route.ts
@@ -9,24 +9,28 @@ export async function POST(request: NextRequest) {
   try {
     if (body.type === "checkout.session.completed") {
       const id = nanoid(10);
-      const name = body.data.object.custom_fields.find(
-        (field: any) => field.key === "name"
-      )?.text.value;
+      const name =
+        body.data.object.custom_fields.find(
+          (field: any) => field.key === "name"
+        )?.text.value || "";
       const email = body.data.object.customer_details.email;
-      const phone = body.data.object.customer_details.phone;
-      const instagram = body.data.object.custom_fields.find(
-        (field: any) => field.key.toLowerCase() === "instagram"
-      )?.text.value;
+      const phone = body.data.object.customer_details.phone || "";
+      const instagram =
+        body.data.object.custom_fields.find(
+          (field: any) => field.key.toLowerCase() === "instagram"
+        )?.text.value || "";
 
       console.log("data to pass", { id, name, email, phone, instagram });
 
-      db.insert(ticketTable).values({
+      const res = await db.insert(ticketTable).values({
         id,
         name,
         email,
         phone,
         instagram,
       });
+
+      console.log(res);
     }
   } catch (error) {
     console.error("Error in stripe_hook route:", error);
