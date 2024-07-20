4c90692c25d90b87fd9a2d39e494f3c659d5350f
serhiihaniuk <serhii.haniuk@gmail.com>
2024-07-20 20:48:57 +0200

    add new event

--- src/app/api/stripe_hook/route.ts
+++ src/app/api/stripe_hook/route.ts
@@ -1,41 +1,96 @@
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
-  typescript: true,
-});
-const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
-
 export async function POST(request: NextRequest) {
-  const body = await request.text();
-  console.log("body", body);
-  const signature = request.headers.get("stripe-signature") || "";
-  let event: Stripe.Event;
+  const body = await request.json();
 
-  try {
-    console.log("trying to construct event", body, signature);
-    event = stripe.webhooks.constructEvent(body, signature, webhookSecret);
-    console.log("constructed event", event);
-  } catch (err: any) {
-    console.error(
-      `Webhook signature verification failed: ${
-        err instanceof Error ? err.message : err
-      }`
-    );
-    return NextResponse.json({ error: err.message }, { status: 400 });
-  }
-
-  console.log("event constructed successfully");
-
-  if (event.type === "payment_intent.succeeded") {
-    const paymentIntent = event.data.object as Stripe.PaymentIntent;
-    console.log(`PaymentIntent ${paymentIntent.id} was successful!`);
+  if (body.type === "payment_intent.succeeded") {
+    const paymentIntent = body.data;
 
     // TODO: Handle the successful payment
+  }
+
+  if (body.type === "checkout.session.completed") {
+    const session = body.data;
+
+    console.log(body.data);
   }
 
   // Handle other event types if needed
 
   return NextResponse.json({ received: true });
 }
+
+/*
+  body {
+  "id": "evt_3PehuPFrJFBf1WKu1RX5eP2F",
+  "object": "event",
+  "api_version": "2024-06-20",
+  "created": 1721499691,
+  "data": {
+    "object": {
+      "id": "pi_3PehuPFrJFBf1WKu18mH4sIU",
+      "object": "payment_intent",
+      "amount": 300,
+      "amount_capturable": 0,
+      "amount_details": {
+        "tip": {
+        }
+      },
+      "amount_received": 300,
+      "application": null,
+      "application_fee_amount": null,
+      "automatic_payment_methods": null,
+      "canceled_at": null,
+      "cancellation_reason": null,
+      "capture_method": "automatic",
+      "client_secret": "pi_3PehuPFrJFBf1WKu18mH4sIU_secret_crUyvB1TNspX6Xtck8lwJ896y",
+      "confirmation_method": "automatic",
+      "created": 1721499689,
+      "currency": "pln",
+      "customer": null,
+      "description": null,
+      "invoice": null,
+      "last_payment_error": null,
+      "latest_charge": "ch_3PehuPFrJFBf1WKu1TIwlQB9",
+      "livemode": true,
+      "metadata": {
+      },
+      "next_action": null,
+      "on_behalf_of": null,
+      "payment_method": "pm_1PehuPFrJFBf1WKuDgIFfEUf",
+      "payment_method_configuration_details": null,
+      "payment_method_options": {
+        "card": {
+          "installments": null,
+          "mandate_options": null,
+          "network": null,
+          "request_three_d_secure": "automatic"
+        }
+      },
+      "payment_method_types": [
+        "card"
+      ],
+      "processing": null,
+      "receipt_email": null,
+      "review": null,
+      "setup_future_usage": null,
+      "shipping": null,
+      "source": null,
+      "statement_descriptor": null,
+      "statement_descriptor_suffix": null,
+      "status": "succeeded",
+      "transfer_data": null,
+      "transfer_group": null
+    }
+  },
+  "livemode": true,
+  "pending_webhooks": 2,
+  "request": {
+    "id": "req_TYwirSatVkwJvI",
+    "idempotency_key": "805cf014-ca2a-4465-ad0c-2b519ab90ac1"
+  },
+  "type": "payment_intent.succeeded"
+}
+
+*/
