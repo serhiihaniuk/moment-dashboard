e98cf5b905398e897e54ea6312669f461c0aed77
serhiihaniuk <serhii.haniuk@gmail.com>
2024-07-20 21:35:34 +0200

    add nanoid

--- src/app/api/stripe_hook/route.ts
+++ src/app/api/stripe_hook/route.ts
@@ -1,29 +1,24 @@
 import { db } from "@/shared/db";
 import { ticketTable } from "@/shared/schema";
 import { NextRequest, NextResponse } from "next/server";
+import { nanoid } from "nanoid";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
 
   try {
     if (body.type === "checkout.session.completed") {
-      console.log(body.data);
-      console.log("body.data.object", body.data.object);
-      console.log(body.data.object.custom_fields);
-
-      const id = body.data.object.id;
-      const name = body.data.object.customer_details.name;
+      const id = nanoid(10);
+      const name = body.data.object.custom_fields.find(
+        (field: any) => field.key === "name"
+      )?.text.value;
       const email = body.data.object.customer_details.email;
       const phone = body.data.object.customer_details.phone;
-      const instagram = body.data.object.custom_fields[0]?.text.value;
-
-      console.log(
-        "custom fields",
-        body.data.object,
-        body.data.object.custom_fields
+      const instagram = body.data.object.custom_fields.find(
+        (field: any) => field.key.toLowerCase() === "instagram"
       );
 
-      console.log({ id, name, email, phone, instagram });
+      console.log("data to pass", { id, name, email, phone, instagram });
 
       db.insert(ticketTable).values({
         id,
