0c964c8459e7f867846b896e0d7e8b66e002359b
serhiihaniuk <serhii.haniuk@gmail.com>
2024-07-20 20:18:55 +0200

    try body text

--- src/app/api/stripe_hook/route.ts
+++ src/app/api/stripe_hook/route.ts
@@ -1,54 +1,41 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  console.error("STRIPE_SECRET_KEY is not defined");
-  throw new Error("STRIPE_SECRET_KEY is not defined");
-}
-
-if (!process.env.STRIPE_WEBHOOK_SECRET) {
-  console.error("STRIPE_WEBHOOK_SECRET is not defined");
-  throw new Error("STRIPE_WEBHOOK_SECRET is not defined");
-}
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2024-06-20",
+  typescript: true,
 });
+const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
-const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
-
-export const config = {
-  bodyParser: false,
-};
-
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   const body = await request.text();
-  const signature = request.headers.get("stripe-signature")!;
-
+  console.log("body", body);
+  const signature = request.headers.get("stripe-signature") || "";
   let event: Stripe.Event;
-  console.log("starting");
 
   try {
-    console.log("trying to construct event");
+    console.log("trying to construct event", body, signature);
     event = stripe.webhooks.constructEvent(body, signature, webhookSecret);
-    console.log("constructed event");
+    console.log("constructed event", event);
   } catch (err: any) {
-    console.error(`Webhook signature verification failed: ${err.message}`);
-    return NextResponse.json({ error: "Invalid signature" }, { status: 400 });
+    console.error(
+      `Webhook signature verification failed: ${
+        err instanceof Error ? err.message : err
+      }`
+    );
+    return NextResponse.json({ error: err.message }, { status: 400 });
   }
 
+  console.log("event constructed successfully");
+
   if (event.type === "payment_intent.succeeded") {
-    console.log("payment intent succeeded");
     const paymentIntent = event.data.object as Stripe.PaymentIntent;
     console.log(`PaymentIntent ${paymentIntent.id} was successful!`);
 
     // TODO: Handle the successful payment
-    // For example, you might want to:
-    // - Update your database
-    // - Send a confirmation email
-    // - Fulfill the order
   }
-  console.log("ending");
+
+  // Handle other event types if needed
 
   return NextResponse.json({ received: true });
 }
