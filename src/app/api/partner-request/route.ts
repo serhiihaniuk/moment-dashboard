47d3297b5bdb7e84133baf6d7a33d2c9d58b4380
serhiihaniuk <serhii.haniuk@gmail.com>
2024-08-18 18:13:58 +0200

    add cors lib

--- src/app/api/partner-request/route.ts
+++ src/app/api/partner-request/route.ts
@@ -1,16 +1,15 @@
+import cors from "@/shared/cors";
 import { db } from "@/shared/db";
 import { partnerRequestTable } from "@/shared/schema";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
-export async function OPTIONS() {
-  return new NextResponse(null, {
-    status: 204,
-    headers: {
-      "Access-Control-Allow-Origin": "https://nailmoment.pl",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    },
-  });
+export async function OPTIONS(request: Request) {
+  return cors(
+    request,
+    new Response(null, {
+      status: 204,
+    })
+  );
 }
 
 export async function POST(request: NextRequest) {
@@ -18,17 +17,13 @@ export async function POST(request: NextRequest) {
     const body = await request.json();
     const { full_name, phone, email, brand_name } = body;
 
-    const headers = new Headers({
-      "Access-Control-Allow-Origin": "https://nailmoment.pl",
-      "Access-Control-Allow-Methods": "POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    });
-
     // Validate required fields
     if (!full_name || !phone || !email || !brand_name) {
-      return NextResponse.json(
-        { error: "Missing required fields" },
-        { status: 400, headers }
+      return cors(
+        request,
+        new Response(null, {
+          status: 400,
+        })
       );
     }
 
@@ -40,15 +35,19 @@ export async function POST(request: NextRequest) {
       brand_name,
     });
 
-    return NextResponse.json({
-      message: "Partner request submitted successfully",
-      headers,
-    });
+    return cors(
+      request,
+      new Response(null, {
+        status: 200,
+      })
+    );
   } catch (error) {
     console.error("Error in partner request route:", error);
-    return NextResponse.json(
-      { error: "Internal server error", message: (error as Error).message },
-      { status: 500 }
+    return cors(
+      request,
+      new Response(null, {
+        status: 500,
+      })
     );
   }
 }
